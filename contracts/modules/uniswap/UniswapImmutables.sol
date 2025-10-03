22de593406bc1fe15cc28cadca1c506d739106ae
Arvin Morawej <arvin.morawej@gmail.com>
2025-10-03 10:16:10 -0400

    feat: Add other protocols hash addresses

--- contracts/modules/uniswap/UniswapImmutables.sol
+++ contracts/modules/uniswap/UniswapImmutables.sol
@@ -6,6 +6,15 @@ struct UniswapParameters {
     address v3Factory;
     bytes32 pairInitCodeHash;
     bytes32 poolInitCodeHash;
+    /// @notice Aerodrome
+    address aerodromeFactory;
+    bytes32 aerodromePoolInitCodeHash;
+    /// @notice Pancake
+    address pancakeFactory;
+    bytes32 pancakePoolInitCodeHash;
+    /// @notice Sushi
+    address sushiFactory;
+    bytes32 sushiPoolInitCodeHash;
 }
 
 contract UniswapImmutables {
@@ -21,10 +30,38 @@ contract UniswapImmutables {
     /// @notice The UniswapV3Pool initcodehash
     bytes32 internal immutable UNISWAP_V3_POOL_INIT_CODE_HASH;
 
+    /// @notice The address of AerodromeV3Factory
+    address internal immutable AERODROME_V3_FACTORY;
+
+    /// @notice The AerodromeV3Pool initcodehash
+    bytes32 internal immutable AERODROME_V3_POOL_INIT_CODE_HASH;
+
+    /// @notice The address of PancakeV3Factory
+    address internal immutable PANCAKE_V3_FACTORY;
+
+    /// @notice The PancakeV3Pool initcodehash
+    bytes32 internal immutable PANCAKE_V3_POOL_INIT_CODE_HASH;
+
+    /// @notice The address of SushiV3Factory
+    address internal immutable SUSHI_V3_FACTORY;
+
+    /// @notice The SushiV3Pool initcodehash
+    bytes32 internal immutable SUSHI_V3_POOL_INIT_CODE_HASH;
+
     constructor(UniswapParameters memory params) {
+        /// @notice Uniswap
         UNISWAP_V2_FACTORY = params.v2Factory;
         UNISWAP_V2_PAIR_INIT_CODE_HASH = params.pairInitCodeHash;
         UNISWAP_V3_FACTORY = params.v3Factory;
         UNISWAP_V3_POOL_INIT_CODE_HASH = params.poolInitCodeHash;
+        /// @notice Aerodrome
+        AERODROME_V3_FACTORY = params.aerodromeFactory;
+        AERODROME_V3_POOL_INIT_CODE_HASH = params.aerodromePoolInitCodeHash;
+        /// @notice Pancake
+        PANCAKE_V3_FACTORY = params.pancakeFactory;
+        PANCAKE_V3_POOL_INIT_CODE_HASH = params.pancakePoolInitCodeHash;
+        /// @notice Sushi
+        SUSHI_V3_FACTORY = params.sushiFactory;
+        SUSHI_V3_POOL_INIT_CODE_HASH = params.sushiPoolInitCodeHash;
     }
 }
